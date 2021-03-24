23309cc8396f812685a347c059696c355e1ba316
Erica Siegel <siegel.erica@gmail.com>
2021-03-23 22:38:25 -0700

    added profile and friend's list routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,16 @@ import React from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
 // A React component we will use to get that data from the above when we're ready to use it
 import ApolloClient from 'apollo-boost';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 // establish the connection to the back-end server's /graphql endpoint
 // establish a new connection to the GraphQL server using Apollo
@@ -25,13 +30,24 @@ function App() {
     // we wrap the entire JSX code with <ApolloProvider> because we're passing the client variable in as the value for the client prop in the provider
     // everything between the JSX tags will eventually have access to the server's API data through the client we set up
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Home />
+      {/* wrap the div elements in a Router component  which makes all of the child components on the page aware of the client side routing */}
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile/:username?" component={Profile} />
+              <Route exact path="/thought/:id" component={SingleThought} />
+
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 // ThoughtList will recieve to props: the title and thoughts array.
 // we destructure it to avoid using props.thoughts and props.title
 const ThoughtList = ({ thoughts, title }) => {
@@ -16,16 +16,21 @@ const ThoughtList = ({ thoughts, title }) => {
                     // key prop helps React internally track which data needs to be re-rendered if something changes
                     <div key={thought._id} className="card mb-3">
                         <p className="card-header">
-                            {thought.username}
+                            <Link
+                                to={`/profile/${thought.username}`}
+                                style={{ fontWeight: 700 }}
+                                className='text-light'
+                            >{thought.username}</Link>{' '}
                             thought on {thought.createdAt}
                         </p>
                         <div className="card-body">
-                            <p>{thought.thoughtText}</p>
-                            <p className="mb-0">
-                                {/* call to action: If there are no reactions, the user will start the discussion by adding the first reaction. If there are reactions, the user will view or add their own reaction to an existing list. */}
-                                Reactions: {thought.reactionCount} || Click to{' '}
-                                {thought.reactionCount ? 'see' : 'start'} the discussion!
-                            </p>
+                            <Link to={`/thought/${thought._id}`}>
+                                <p>{thought.thoughtText}</p>
+                                <p className="mb-0">
+                                    Reactions: {thought.reactionCount} || Click to{' '}
+                                    {thought.reactionCount ? 'see' : 'start'} the discussion!
+                                </p>
+                            </Link>
                         </div>
                     </div>
                 ))
